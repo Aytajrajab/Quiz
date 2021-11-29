cad5703895121f33190892259f8dd01e011d071d
Aytajrajab <88050466+Aytajrajab@users.noreply.github.com>
2021-11-30 03:39:46 +0400

    Add files via upload

---
+++ NewTaskBackEnd/NewTaskBackEnd/Controllers/About.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewTaskBackEnd.Controllers
+{
+    public class About : Controller
+    {
+        public string Index()
+        {
+            return "About us";
+        }
+    }
+}
